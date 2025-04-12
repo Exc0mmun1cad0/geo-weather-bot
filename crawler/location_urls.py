af0f39fdaf8c3558ca3e05305ccc382c93f963fd
Exc0mmun1cad0 <fauxmen4@gmail.com>
2025-04-12 18:06:25 +0300

    refactor getting location urls

--- crawler/location_urls.py
+++ crawler/location_urls.py
@@ -3,27 +3,24 @@ import urllib.parse
 import csv
 from bs4 import BeautifulSoup
 
-earthcam_url = "https://www.earthcam.com/network/"
-csv_headers = ["Location", "URL"]
 
-# Get html content of "network" page
-resp = requests.get(url=earthcam_url)
-data = resp.content.decode("utf-8")
+def get_location_urls() -> list[str]:
+    earthcam_url = "https://www.earthcam.com/network/"
+    csv_headers = ["Location", "URL"]
 
-# Init soup object from bs4 lib
-soup = BeautifulSoup(data, "lxml")
+    # Get html content of "network" page
+    resp = requests.get(url=earthcam_url)
+    data = resp.content.decode("utf-8")
 
-# Get elements from sideList which contain link
-side_list = soup.find("div", {"class": "sideList"})
-elements = side_list.find_all("p", {"class": "location"})
+    # Init soup object from bs4 lib
+    soup = BeautifulSoup(data, "lxml")
 
-# Extract link from every element and write it to csv with location name
-with open("locations.csv", "w", encoding="utf-8") as f:
-    # Create csv file
-    writer = csv.writer(f, lineterminator="\r")
-    writer.writerow(csv_headers)
+    # Get elements from sideList which contain link
+    side_list = soup.find("div", {"class": "sideList"})
+    elements = side_list.find_all("p", {"class": "location"})
 
-    # Iterate over elements, write formed url and location to csv
+    camera_urls = []
+
     for el in elements:
         location_link = el.find("span")
         location = location_link.text
@@ -31,4 +28,6 @@ with open("locations.csv", "w", encoding="utf-8") as f:
         path = (location_link.find("a").get("href"))[len("index.php?"):]
         url = earthcam_url + "?" + path
 
-        writer.writerow([location, url])
+        camera_urls.append(url)
+
+    return camera_urls
