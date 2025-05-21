2e6786c55fc16fac0b339937540a19029e8be3f3
unknown <125723625+Zalupen227@users.noreply.github.com>
2025-05-21 21:21:10 +0300

    add classifier

--- scripts/model_training/weather_classification.py
+++ scripts/model_training/weather_classification.py
@@ -1,72 +1,70 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torchvision import transforms, models, datasets  # Добавлен импорт datasets
+import matplotlib.pyplot as plt
+from torchvision import transforms, models, datasets
 from torch.utils.data import DataLoader, random_split
 from pathlib import Path
+
+from torchvision.models import ConvNeXt_Tiny_Weights
 from tqdm import tqdm
 
 
-# Конфигурация
 class Config:
     data_dir = "classification dataset"
-    classes = ["-10 - 0", "-30 - -10", "0 - 10", "10 - 30", "30 - 50"]
-    batch_size = 32
-    num_epochs = 15
-    lr = 0.001
+    classes = ["-10 - 0", "-30 - -10", "0 - 10", "10 - 30"]
+    batch_size = 8
+    num_epochs = 5
+    lr = 0.001  # learning rate
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-
-# Проверка датасета
-print("Проверка структуры датасета:")
-for cls in Config.classes:
-    path = Path(Config.data_dir) / cls
-    count = sum(1 for _ in path.glob('*/*') if _.is_file())
-    print(f"{cls}: {count} изображений")
-
-# Трансформации
-transform = transforms.Compose([
-    transforms.Resize((224, 224)),
-    transforms.RandomHorizontalFlip(),
-    transforms.ToTensor(),
-    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-])
-
-# Загрузка данных
-try:
-    full_dataset = datasets.ImageFolder(root=Config.data_dir, transform=transform)
-    print("\nСоответствие классов:", full_dataset.class_to_idx)
-except Exception as e:
-    print(f"\nОшибка загрузки данных: {e}")
-    exit()
-
-# Разделение данных
-train_size = int(0.8 * len(full_dataset))
-val_size = len(full_dataset) - train_size
-train_dataset, val_dataset = random_split(full_dataset, [train_size, val_size])
-
-train_loader = DataLoader(train_dataset, batch_size=Config.batch_size, shuffle=True, num_workers=4)
-val_loader = DataLoader(val_dataset, batch_size=Config.batch_size, shuffle=False, num_workers=4)
-
-# Модель
-model = models.efficientnet_b0(weights=models.EfficientNet_B0_Weights.DEFAULT)
-model.classifier[1] = nn.Linear(model.classifier[1].in_features, len(Config.classes))
-model = model.to(Config.device)
-
-# Оптимизатор и критерий
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr=Config.lr)
-scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=5, gamma=0.1)
+    # Transformations (according to the ImageNet)
+    transform = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),  # convert to pytorch tensor and normalise pixel values
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        # normalization of the image using RGB channels
+    ])
 
 
-# Обучение
-def train():
+def print_dataset_structure():
+    print("Dataset structure:")
+    for cls in Config.classes:
+        path = Path(Config.data_dir) / cls
+        count = sum(1 for _ in path.glob('*/*') if _.is_file())
+        print(f"{cls}: {count} images")
+
+
+# Data load
+def load_data(transform):
+    try:
+        full_dataset = datasets.ImageFolder(root=Config.data_dir, transform=transform)
+        print("\nDict: class -> num_images:", full_dataset.class_to_idx)
+    except Exception as e:
+        print(f"\nData load err: {e}")
+        exit()
+
+    return full_dataset
+
+
+def split_data(full_dataset):
+    # Data split (train/val)
+    train_size = int(0.8 * len(full_dataset))
+    val_size = len(full_dataset) - train_size
+    train_dataset, val_dataset = random_split(full_dataset, [train_size, val_size])
+    return train_dataset, val_dataset
+
+
+def train(model, train_loader, val_loader, optimizer, criterion, scheduler):
     best_acc = 0.0
+    train_losses = []
+    train_accuracies = []
+
     for epoch in range(Config.num_epochs):
         print(f"\nEpoch {epoch + 1}/{Config.num_epochs}")
-
-        # Тренировка
         model.train()
+
         train_loss = 0.0
         correct = 0
         total = 0
@@ -74,20 +72,26 @@ def train():
         for inputs, labels in tqdm(train_loader, desc="Training"):
             inputs, labels = inputs.to(Config.device), labels.to(Config.device)
 
-            optimizer.zero_grad()
+            optimizer.zero_grad()  # set grad to zero
             outputs = model(inputs)
             loss = criterion(outputs, labels)
-            loss.backward()
-            optimizer.step()
+            loss.backward()  # back propagation
+            optimizer.step()  # renew model weights
 
             train_loss += loss.item()
             _, predicted = outputs.max(1)
             total += labels.size(0)
             correct += predicted.eq(labels).sum().item()
 
-        train_acc = 100 * correct / total
+        epoch_loss = train_loss / len(train_loader)
+        epoch_acc = 100 * correct / total
 
-        # Валидация
+        train_losses.append(epoch_loss)
+        train_accuracies.append(epoch_acc)
+
+        print(f"Train Los.s: {epoch_loss:.4f} | Train Accuracy: {epoch_acc:.2f}%")
+
+        # Validation
         model.eval()
         val_loss = 0.0
         correct = 0
@@ -106,7 +110,7 @@ def train():
 
         val_acc = 100 * correct / total
 
-        print(f"Train Loss: {train_loss / len(train_loader):.3f} | Acc: {train_acc:.2f}%")
+        print(f"Train Loss: {train_loss / len(train_loader):.3f} | Acc: {epoch_acc:.2f}%")
         print(f"Val Loss: {val_loss / len(val_loader):.3f} | Acc: {val_acc:.2f}%")
 
         if val_acc > best_acc:
@@ -117,6 +121,72 @@ def train():
 
     print(f"\nBest Validation Accuracy: {best_acc:.2f}%")
 
+    return train_losses, train_accuracies
+
+
+def plot_results(train_losses, train_accuracies):
+    # plot graphs
+    plt.figure(figsize=(10, 4))
+
+    # Loss
+    plt.subplot(1, 2, 1)
+    plt.plot(train_losses, label="Loss", color="red")
+    plt.title("Training Loss")
+    plt.xlabel("Epoch")
+    plt.ylabel("Loss")
+    plt.grid(True)
+
+    # Accuracy
+    plt.subplot(1, 2, 2)
+    plt.plot(train_accuracies, label="Accuracy", color="green")
+    plt.title("Training Accuracy")
+    plt.xlabel("Epoch")
+    plt.ylabel("Accuracy (%)")
+    plt.grid(True)
+
+    plt.tight_layout()
+    plt.savefig("figures\\convnext_tiny.png")
+    plt.show()
+
+
+def main():
+    print_dataset_structure()
+
+    # Transformations (according to the ImageNet)
+    transform = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),  # convert to pytorch tensor and normalise pixel values
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        # normalization of the image using RGB channels
+    ])
+
+    full_dataset = load_data(Config.transform)
+
+    train_dataset, val_dataset = split_data(full_dataset)
+
+    train_loader = DataLoader(train_dataset, batch_size=Config.batch_size, shuffle=True, num_workers=4)
+    val_loader = DataLoader(val_dataset, batch_size=Config.batch_size, shuffle=False, num_workers=4)
+
+    # Model
+    # model = models.efficientnet_v2_m(weights=models.EfficientNet_V2_M_Weights.DEFAULT)
+    # model = models.efficientnet_b0(weights=models.EfficientNet_B0_Weights.DEFAULT)
+    # model = models.wide_resnet101_2(weights=models.Wide_ResNet101_2_Weights.DEFAULT)
+    # model = models.mobilenet_v3_large(weights=models.MobileNet_V3_Large_Weights.DEFAULT)
+    # model = models.densenet161(weights=models.DenseNet161_Weights.DEFAULT)
+    model = models.convnext_tiny(weights=ConvNeXt_Tiny_Weights.IMAGENET1K_V1)
+    # in_features - number of features to the last layer's neuron
+    model.classifier[2] = nn.Linear(model.classifier[2].in_features, len(Config.classes))
+    model = model.to(Config.device)
+
+    # Optimizer and Criterion
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=Config.lr)
+    # Scheduler controls learning rate
+    scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=5, gamma=0.1)
+
+    train_losses, train_accuracies = train(model, train_loader, val_loader, optimizer, criterion, scheduler)
+    plot_results(train_losses, train_accuracies)
 
 if __name__ == "__main__":
-    train()
+    main()
